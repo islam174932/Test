fbea2d89822c91073527a5a66db0027464cfe9dc
islam174932 <eslamgamal19933@gmail.com>
2024-06-16 01:13:09 +0200

    first commit

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,66 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        // Set up the WebDriver and configure the browser
+        System.setProperty("webdriver.chrome.driver", "C:/Users/User/Desktop/Chrome/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.opencart.com/"); // Replace with the actual URL of your website
+    }
+
+    @Test
+    public void testInvalidLogin() throws InterruptedException {
+        // The user is on the main page of the website and gets the title of the page
+        System.out.println("The user is on the main page: " + driver.getTitle());
+
+        // The user will click on Account
+        Thread.sleep(3000);
+        WebElement account = driver.findElement(By.xpath("//*[@id=\"customer_menu_top\"]/li/a"));
+        account.click();
+        Thread.sleep(3000);
+
+        // The user will write invalid username and password
+        String username = "invalidUsername";
+        String password = "invalidPassword";
+        enterInvalidCredentials(username, password);
+
+        // The user will click the sign-in button
+        Thread.sleep(3000);
+        WebElement signInButton = driver.findElement(By.xpath("//*[@id=\"loginFrm\"]/fieldset/button"));
+        signInButton.click();
+        Thread.sleep(7000);
+
+        // Add assertions to verify the expected outcomes, if any
+    }
+
+    private void enterInvalidCredentials(String username, String password) throws InterruptedException {
+        WebElement usernameField = driver.findElement(By.xpath("//*[@id=\"loginFrm_loginname\"]"));
+        usernameField.sendKeys(username);
+        WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"loginFrm_password\"]"));
+        passwordField.sendKeys(password);
+        Thread.sleep(3000);
+    }
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
